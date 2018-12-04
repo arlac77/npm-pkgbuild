5e77738f1b2076ba6ee6dac17f96ebe56942b8b3
Markus Felten <Markus.Felten@gmx.de>
2018-12-04 23:41:28 +0100

    feat: call execa makepkg ...

--- src/npm-pkgbuild-cli.mjs
+++ src/npm-pkgbuild-cli.mjs
@@ -2,12 +2,29 @@ import { version } from "../package.json";
 import { npm2pkgbuild } from "./npm-pkgbuild";
 import { createWriteStream } from "fs";
 import program from "caporal";
+import { join } from "path";
+import execa from "execa";
+const { promises } = require("fs");
 
 program
   .description("create arch linux package from npm")
   .version(version)
+  .option("-w <dir>")
   .action(async (args, options, logger) => {
-    npm2pkgbuild(process.cwd(), process.stdout);
+    if (options.w !== undefined) {
+      const wd = options.w;
+      const dest = createWriteStream(join(wd, "PKGBUILD"));
+      promises.mkdir(wd, { recursive: true });
+
+      npm2pkgbuild(process.cwd(), dest);
+
+      const r = await execa("makepkg", ["-f"], { cwd: wd });
+      console.log(r.stderr);
+      console.log(r.stdout);
+    }
+    else {
+      npm2pkgbuild(process.cwd(), process.stdout);
+    }
   });
 
 program.parse(process.argv);

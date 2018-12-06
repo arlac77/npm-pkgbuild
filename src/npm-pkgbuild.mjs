f54e275651a13c1cdf2484d3657f4292008b272e
Markus Felten <Markus.Felten@gmx.de>
2018-12-06 20:40:43 +0100

    fix: use stream finished

--- src/npm-pkgbuild.mjs
+++ src/npm-pkgbuild.mjs
@@ -1,5 +1,7 @@
 import { createWriteStream } from "fs";
 import { join } from "path";
+import { promisify } from 'util';
+import { finished } from 'stream';
 import fs from "fs";
 
 export async function npm2pkgbuild(dir, out) {
@@ -44,21 +46,63 @@ pkgver() {
   printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
 }
 
-package() {
+build() {
   cd "${pkg.name}"
   npm install
   npm install --production
   npm prune
+  find . -name "*~" -print0|xargs -r -0 rm
+  find node_modules -name "*.1" -print0|xargs -r -0 rm
+  find node_modules -name "*.patch" -print0|xargs -r -0 rm
+  find node_modules -iname test -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname tests -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname doc -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname docs -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname dist -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname example -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname examples -type d -print0|xargs -r -0 rm -rf
+  find node_modules -iname "readme*" -print0|xargs -r -0 rm
+  find node_modules -iname "AUTHORS*" -print0|xargs -r -0 rm
+  find node_modules -iname "NOTICE*" -print0|xargs -r -0 rm
+  find node_modules -iname "HISTORY*" -print0|xargs -r -0 rm
+  find node_modules -iname "SUMMARY.md" -print0|xargs -r -0 rm
+  find node_modules -iname "PULL_REQUEST_TEMPLATE.md" -print0|xargs -r -0 rm
+  find node_modules -iname "CONTRIBUTING*" -print0|xargs -r -0 rm
+  find node_modules -iname "Contributors*" -print0|xargs -r -0 rm
+  find node_modules -iname "CHANGES*" -print0|xargs -r -0 rm
+  find node_modules -iname "CHANGELOG*" -print0|xargs -r -0 rm
+  find node_modules -iname "Makefile*" -print0|xargs -r -0 rm
+  find node_modules -name "*.bash_completion.*" -print0|xargs -r -0 rm
+  find . -name "jsdoc.json" -print0|xargs -r -0 rm
+  find . -name "SECURITY.md" -print0|xargs -r -0 rm
+  find . -name "SFTPStream.md" -print0|xargs -r -0 rm
+  find . -name "LIMITS.md" -print0|xargs -r -0 rm
+  find . -name ".gitignore" -print0|xargs -r -0 rm
+  find . -name ".gitmodules" -print0|xargs -r -0 rm
+  find . -name ".verb.md" -print0|xargs -r -0 rm
+  find . -name ".npmignore" -print0|xargs -r -0 rm
+  find . -name ".travis.yml" -print0|xargs -r -0 rm
+  find . -name ".jshintrc" -print0|xargs -r -0 rm
+  find . -name ".eslintrc" -print0|xargs -r -0 rm
+  find . -name ".eslintignore" -print0|xargs -r -0 rm
+  find . -name ".nvmrc" -print0|xargs -r -0 rm
+  find . -name ".zuul.yml" -print0|xargs -r -0 rm
+  find . -name ".doclets.yml" -print0|xargs -r -0 rm
+  find . -name ".editorconfig" -print0|xargs -r -0 rm
+  find . -name ".gitattributes" -print0|xargs -r -0 rm
+  find . -name ".tern-project" -print0|xargs -r -0 rm
+  find . -name ".dockerignore" -print0|xargs -r -0 rm
+  find . -name ".dir-locals.el" -print0|xargs -r -0 rm
+}
 
+package() {
   ${Object.keys(pkg.bin||{}).map(n => `install -Dm755 ${pkg.bin[n]} "\${pkgdir}/${pkg.bin[n]}"`).join('\n  ')}
   install node_modules "\${pkgdir}/node_modules"
 }
 `
   );
 
-  await new Promise((resolve,reject) => {
-    out.on('close', resolve);
-    out.on('error', reject);
-    out.end();
-  });
+  out.end();
+
+  await promisify(finished);
 }

bd4c07a8f85bcb77a2bf0106ef137c9a0319580a
Markus Felten <Markus.Felten@gmx.de>
2019-01-19 20:08:20 +0100

    fix: remove --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [
         file: pkg.bin[name],
         format: "cjs",
         banner:
-          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-worker "$0" "$@"',
+          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
         interop: false
       },
       external,

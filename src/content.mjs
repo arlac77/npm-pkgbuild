5f152299bcac56d14a3c3d41845571d419c565d6
Markus Felten <Markus.Felten@db.com>
2019-02-20 15:46:58 +0100

    refactor: use endsWith() to check for '/'

--- src/content.mjs
+++ src/content.mjs
@@ -18,7 +18,7 @@ export async function content(context, stagingDir) {
         for (const name of await globby(asArray(source), {
           cwd: context.dir
         })) {
-          const d = dest[dest.length -1] === '/' ? join(dest,name) : dest;
+          const d = dest.endsWith('/') ? join(dest,name) : dest;
           await fs.promises.mkdir(dirname(d), { recursive: true });
 
           await copyTemplate(context, join(context.dir, name), d);

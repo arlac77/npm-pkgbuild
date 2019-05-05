e512cd5d778c5ef84f5d6f85c665db1613c3dc43
Markus Felten <Markus.Felten@gmx.de>
2019-05-06 01:01:36 +0200

    fix: handle missing pkg.pacman

--- src/pacman.mjs
+++ src/pacman.mjs
@@ -5,13 +5,15 @@ import { copyTemplate } from "./util.mjs";
 export async function pacman(context, stagingDir) {
   const pkg = context.pkg;
 
-  if (pkg.pacman.install !== undefined) {
-    await fs.promises.mkdir(stagingDir, { recursive: true });
+  if (pkg.pacman !== undefined) {
+    if (pkg.pacman.install !== undefined) {
+      await fs.promises.mkdir(stagingDir, { recursive: true });
 
-    await copyTemplate(
-      context,
-      join(context.dir, pkg.pacman.install),
-      join(stagingDir, `${pkg.name}.install`)
-    );
+      await copyTemplate(
+        context,
+        join(context.dir, pkg.pacman.install),
+        join(stagingDir, `${pkg.name}.install`)
+      );
+    }
   }
 }

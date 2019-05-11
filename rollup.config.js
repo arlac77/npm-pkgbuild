c454c458354925235c2d4156a883b304cf042103
Markus Felten <Markus.Felten@gmx.de>
2019-05-11 14:19:23 +0200

    chore(package): c8@^4.1.5 chore(package): rollup-plugin-node-resolve@^4.2.4 chore(package): add builtin-modules@^3.1.0 chore(scripts): cover@#overwrite c8 --temp-directory build/tmp ava && c8 report -r lcov -o build/coverage --temp-directory build/tmp chore(package): add nyc from template chore(package): set $.ava.require='esm' as in template chore(package): set $.ava.files='tests/*-test.js,tests/*-test.mjs' as in template chore(package): set $.ava.extensions='js,mjs' as in template chore(rollup): import builtins

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import builtins from "builtin-modules";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";

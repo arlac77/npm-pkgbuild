57e38767e20a36b5544a17ef18ebb4d9717e096a
Markus Felten <Markus.Felten@gmx.de>
2019-05-13 12:26:16 +0200

    fix: quote depends values in package() function

--- src/pkgbuild.mjs
+++ src/pkgbuild.mjs
@@ -132,7 +132,7 @@ build() {
 }
 
 package() {
-  depends=(${makeDepends(pkg.pacman.depends)})
+  depends=(${makeDepends(pkg.pacman.depends).map(a => `"${a}"`)})
 
   mkdir -p \${pkgdir}${installdir}
   ${npmDistPackage}

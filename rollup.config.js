54550d65eb5ab903654082288bd57c5cccc4bfba
Markus Felten <Markus.Felten@gmx.de>
2019-05-21 00:40:24 +0200

    chore(rollup):  make use of buildins & cleanum *.mjs too

--- rollup.config.js
+++ rollup.config.js
@@ -6,47 +6,7 @@ import json from "rollup-plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import pkg from "./package.json";
 
-const external = [
-  "assert",
-  "async_hooks",
-  "buffer",
-  "child_process",
-  "cluster",
-  "console",
-  "constants",
-  "crypto",
-  "dgram",
-  "dns",
-  "domain",
-  "events",
-  "fs",
-  "http",
-  "http2",
-  "https",
-  "inspector",
-  "module",
-  "net",
-  "os",
-  "path",
-  "perf_hooks",
-  "process",
-  "punycode",
-  "querystring",
-  "readline",
-  "repl",
-  "stream",
-  "string_decoder",
-  "sys",
-  "timers",
-  "tls",
-  "trace_events",
-  "tty",
-  "url",
-  "util",
-  "v8",
-  "vm",
-  "zlib"
-];
+const external = [...builtins];
 
 export default Object.keys(pkg.bin || {}).map(name => {
   return {
@@ -67,7 +27,9 @@ export default Object.keys(pkg.bin || {}).map(name => {
         preferConst: true,
         compact: true
       }),
-      cleanup(),
+      cleanup({
+        extensions: ["js", "mjs", "jsx", "tag"]
+      }),
       executable()
     ]
   };

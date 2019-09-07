91d030d628b20612eb9e206c814e203c4c1eda55
Markus Felten <Markus.Felten@gmx.de>
2019-09-07 23:58:56 +0200

    feat: exclude blacklisted packages from cleanup
    currently only 'parse-client-options'

--- src/cleanup.mjs
+++ src/cleanup.mjs
@@ -29,128 +29,132 @@ async function iterate(o, cb) {
   }
 }
 
+const blacklist = new Set(["parse-client-options"]);
+
 export async function cleanup(context, stagingDir) {
   for (const name of await globby(["**/package.json"], {
     cwd: stagingDir
   })) {
     const pkgFile = join(stagingDir, name);
-    console.log(`cleanup ${pkgFile}`);
-
     const pkg = JSON.parse(await readFile(pkgFile, utf8StreamOptions));
 
-    // unused files may also be deleted
-    await Promise.all(
-      ["types", "unpkg", "shim", "browser", "testling", "source"].map(
-        async key => {
-          await iterate(pkg[key], async o => rm(join(dirname(pkgFile), o)));
+    if (!blacklist.has(pkg.name)) {
+      console.log(`cleanup ${pkgFile}`);
+
+      // unused files may also be deleted
+      await Promise.all(
+        ["types", "unpkg", "shim", "browser", "testling", "source"].map(
+          async key => {
+            await iterate(pkg[key], async o => rm(join(dirname(pkgFile), o)));
+            delete pkg[key];
+          }
+        )
+      );
+
+      [
+        "version",
+        "name",
+        "dependencies",
+
+        "jspm",
+        "jsnext:main",
+        "man",
+        "files",
+        "directories",
+        "devDependencies",
+        "bundleDependencies",
+        "peerDependencies",
+        "optionalDependencies",
+        "sideEffects",
+        "pika",
+        "private",
+        "publishConfig",
+        "repository",
+        "license",
+        "licenses",
+        "changelog",
+        "keywords",
+        "homepage",
+        "bugs",
+        "scripts",
+        "types",
+        "deprecated",
+        "description",
+        "decription",
+        "engines",
+        "author",
+        "authors",
+        "contributors",
+        "maintainers",
+        "verb",
+        "xo",
+        "prettier",
+        "jest",
+        "remarkConfig",
+        "nyc",
+        "ava",
+        "publishConfig",
+        "typeScriptVersion",
+        "typesPublisherContentHash",
+        "typings",
+        "systemd",
+        "pacman",
+        "lintDeps",
+        "icon",
+        "config",
+        "release",
+        "template",
+        "spm",
+        "precommit.silent",
+        "greenkeeper",
+        "bundlesize",
+        "standard",
+        "ignore",
+        "ender",
+        "dojoBuild",
+        "component",
+        "eslintConfig",
+        "env",
+        "commitlint",
+        "standard-version",
+        "lint-staged",
+        "lintStaged",
+        "ci",
+        "husky",
+        "verbiage",
+        "os",
+        "gypfile",
+        "coordinates",
+        "tap",
+        "typesVersions",
+        "node-gyp-build-optional",
+        "node-gyp-build-test",
+        "gitHead",
+        "hallmark"
+      ].map(key => {
+        delete pkg[key];
+      });
+
+      for (const key of Object.keys(pkg)) {
+        if (key[0] === "_") {
           delete pkg[key];
         }
-      )
-    );
-
-    [
-      "version",
-      "name",
-      "dependencies",
-
-      "jspm",
-      "jsnext:main",
-      "man",
-      "files",
-      "directories",
-      "devDependencies",
-      "bundleDependencies",
-      "peerDependencies",
-      "optionalDependencies",
-      "sideEffects",
-      "pika",
-      "private",
-      "publishConfig",
-      "repository",
-      "license",
-      "licenses",
-      "changelog",
-      "keywords",
-      "homepage",
-      "bugs",
-      "scripts",
-      "types",
-      "deprecated",
-      "description",
-      "decription",
-      "engines",
-      "author",
-      "authors",
-      "contributors",
-      "maintainers",
-      "verb",
-      "xo",
-      "prettier",
-      "jest",
-      "remarkConfig",
-      "nyc",
-      "ava",
-      "publishConfig",
-      "typeScriptVersion",
-      "typesPublisherContentHash",
-      "typings",
-      "systemd",
-      "pacman",
-      "lintDeps",
-      "icon",
-      "config",
-      "release",
-      "template",
-      "spm",
-      "precommit.silent",
-      "greenkeeper",
-      "bundlesize",
-      "standard",
-      "ignore",
-      "ender",
-      "dojoBuild",
-      "component",
-      "eslintConfig",
-      "env",
-      "commitlint",
-      "standard-version",
-      "lint-staged",
-      "lintStaged",
-      "ci",
-      "husky",
-      "verbiage",
-      "os",
-      "gypfile",
-      "coordinates",
-      "tap",
-      "typesVersions",
-      "node-gyp-build-optional",
-      "node-gyp-build-test",
-      "gitHead",
-      "hallmark"
-    ].map(key => {
-      delete pkg[key];
-    });
-
-    for (const key of Object.keys(pkg)) {
-      if (key[0] === "_") {
-        delete pkg[key];
       }
-    }
 
-    switch (pkg.main) {
-      case "index":
-      case "./index":
-      case "index.js":
-      case "./index.js":
-      case "":
-        delete pkg.main;
-    }
+      switch (pkg.main) {
+        case "index":
+        case "./index":
+        case "index.js":
+        case "./index.js":
+        case "":
+          delete pkg.main;
+      }
 
-    if (Object.keys(pkg).length === 0 || pkg.type === "module") {
-      await unlink(pkgFile);
-    } else {
-      await writeFile(pkgFile, JSON.stringify(pkg), utf8StreamOptions);
+      if (Object.keys(pkg).length === 0 || pkg.type === "module") {
+        await unlink(pkgFile);
+      } else {
+        await writeFile(pkgFile, JSON.stringify(pkg), utf8StreamOptions);
+      }
     }
   }
 }

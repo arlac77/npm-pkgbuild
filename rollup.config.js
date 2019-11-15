9576a626f2d38f33b76307b1824645c0e12dc325
Markus Felten <Markus.Felten@gmx.de>
2019-11-15 12:04:36 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,52 @@
-import builtins from "builtin-modules";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
 import pkg from "./package.json";
 
 const external = [...builtins];
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
 
-export default Object.keys(pkg.bin || {}).map(name => {
+const config = Object.keys(pkg.bin || {}).map(name => {
   return {
     input: `src/${name}-cli.mjs`,
     output: {
-      file: pkg.bin[name],
-      format: "cjs",
+      plugins: [executable()],
       banner:
-        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
-      interop: false
-    },
-    external,
-    plugins: [
-      resolve(),
-      commonjs(),
-      json({
-        include: "package.json",
-        preferConst: true,
-        compact: true
-      }),
-      cleanup({
-        extensions: ["js", "mjs", "jsx", "tag"]
-      }),
-      executable()
-    ]
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
   };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined && pkg.module != pkg.main) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { plugins, external, ...c };
 });

f57394c6fdd761ddd467aea1a83a66d93b8033bf
Markus Felten <Markus.Felten@gmx.de>
2020-04-30 23:47:51 +0200

    wip

--- src/content/file-content-provider.mjs
+++ src/content/file-content-provider.mjs
@@ -1,5 +1,36 @@
+import { join, dirname } from "path";
+import fs from "fs";
+import globby from "globby";
+import { asArray, copyTemplate } from "../util.mjs";
+
 import { ContentProvider } from "./content-provider.mjs";
 
 export class FileContentProvider extends ContentProvider {
-  async processContent() {}
+  async processContent(context) {
+    const content = context.expand(this.content);
+
+    await Promise.all(
+      Object.entries(content).map(async ([source, dest]) => {
+        dest = join(stagingDir, dest);
+        let cwd, pattern;
+
+        if (typeof source === "string" || source instanceof String) {
+          cwd = context.dir;
+          pattern = source;
+        } else {
+          cwd = join(context.dir, source.base);
+          pattern = source.pattern || "**/*";
+        }
+
+        for (const name of await globby(asArray(pattern), {
+          cwd
+        })) {
+          const d = dest.endsWith("/") ? join(dest, name) : dest;
+          await fs.promises.mkdir(dirname(d), { recursive: true });
+
+          await copyTemplate(context, join(cwd, name), d);
+        }
+      })
+    );
+  }
 }

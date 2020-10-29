743125d925b3b490c6f28ce9af4fc7db5ebf003f
Markus Felten <Markus.Felten@gmx.de>
2020-10-29 15:03:32 +0100

    wip

--- src/content/file-content-provider.mjs
+++ src/content/file-content-provider.mjs
@@ -1,8 +1,8 @@
-import { join, dirname } from "path";
-import { mkdir }  from "fs/promises";
+import { join } from "path";
+import { FileSystemEntry } from "content-entry-filesystem";
 
 import globby from "globby";
-import { asArray, copyTemplate } from "../util.mjs";
+import { asArray } from "../util.mjs";
 
 import { ContentProvider } from "./content-provider.mjs";
 
@@ -10,31 +10,25 @@ import { ContentProvider } from "./content-provider.mjs";
  * content provided form the file system
  */
 export class FileContentProvider extends ContentProvider {
-  async * entries(context) {
+  async *entries(context) {
     const content = context.expand(this.content);
 
-    await Promise.all(
-      Object.entries(content).map(async ([source, dest]) => {
-        dest = join(stagingDir, dest);
-        let cwd, pattern;
+    Object.entries(content).map(async ([source, dest]) => {
+      let cwd, pattern;
 
-        if (typeof source === "string" || source instanceof String) {
-          cwd = context.dir;
-          pattern = source;
-        } else {
-          cwd = join(context.dir, source.base);
-          pattern = source.pattern || "**/*";
-        }
+      if (typeof source === "string" || source instanceof String) {
+        cwd = context.dir;
+        pattern = source;
+      } else {
+        cwd = join(context.dir, source.base);
+        pattern = source.pattern || "**/*";
+      }
 
-        for (const name of await globby(asArray(pattern), {
-          cwd
-        })) {
-          const d = dest.endsWith("/") ? join(dest, name) : dest;
-          await mkdir(dirname(d), { recursive: true });
-
-          await copyTemplate(context, join(cwd, name), d);
-        }
-      })
-    );
+      for (const name of await globby(asArray(pattern), {
+        cwd
+      })) {
+        yield new FileSystemEntry(name);
+      }
+    });
   }
 }

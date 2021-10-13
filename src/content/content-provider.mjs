93dd21c038130ffb10cf15c522149919a49f1b0e
Markus Felten <Markus.Felten@gmx.de>
2021-10-13 23:57:39 +0200

    wip

--- src/content/content-provider.mjs
+++ src/content/content-provider.mjs
@@ -1,7 +1,12 @@
 
 /**
- * source of package content
+ * Source of package content.
  */
 export class ContentProvider {
+
+  /**
+   * Delivers content entries to be packed.
+   * @param context
+   */
   async * entries(context) {}
 }

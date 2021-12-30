efdfed76cc6c8f150d22ee27075e1778cb837814
Markus Felten <Markus.Felten@gmx.de>
2021-12-30 01:36:51 +0100

    test: without filename from stdout

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -1,7 +1,12 @@
 import test from "ava";
+import { join } from "path";
+import { stat, mkdtemp } from "fs/promises";
+import { tmpdir } from "os";
 import { execa } from "execa";
 
 test("cli --deb", async t => {
+  const destination = await mkdtemp(join(tmpdir(), "cli-deb"));
+
   const p = await execa(
     new URL("../src/npm-pkgbuild-cli.mjs", import.meta.url).pathname,
     [
@@ -10,9 +15,11 @@ test("cli --deb", async t => {
       new URL("fixtures/content", import.meta.url).pathname,
       "-m",
       new URL("fixtures/deb", import.meta.url).pathname,
-      "--destination", "/tmp"
+      "--destination", destination
     ]
   );
 
-  t.true(p.stdout.endsWith("npm-pkgbuild_0.0.0-semantic-release_all.deb"))
+  const fileName = join(destination,"npm-pkgbuild_0.0.0-semantic-release_all.deb");
+  const s = await stat(fileName);
+  t.true(s.size >= 900, `package file size ${s.size}`);
 });

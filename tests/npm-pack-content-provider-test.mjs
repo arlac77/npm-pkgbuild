f9d7f4759614429fa0631efa48f79753df491f23
Markus Felten <Markus.Felten@gmx.de>
2022-01-07 22:15:05 +0100

    test: adopt fixtures

--- tests/npm-pack-content-provider-test.mjs
+++ tests/npm-pack-content-provider-test.mjs
@@ -1,18 +1,15 @@
 import test from "ava";
 import { NPMPackContentProvider } from "npm-pkgbuild";
 
-test.skip("NPMPack entries", async t => {
+test("NPMPack entries", async t => {
   const content = new NPMPackContentProvider({
     dir: new URL("fixtures/pkg", import.meta.url).pathname
   });
 
-  const entries = [];
+  const entries = {};
   for await (const entry of content) {
-    entries.push(entry);
+    entries[entry.name] = entry;
   }
 
-  t.deepEqual(
-    entries.map(e => e.name).filter((x, i) => i < 2),
-    ["LICENSE", "package.json"]
-  );
+  t.truthy(entries['package.json']);
 });

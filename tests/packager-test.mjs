922273e32e14894946128b1fc62de950b8ec4bd8
Markus Felten <Markus.Felten@gmx.de>
2022-01-08 14:20:36 +0100

    test: default values

--- tests/packager-test.mjs
+++ tests/packager-test.mjs
@@ -3,10 +3,17 @@ import { Packager } from "npm-pkgbuild";
 
 class MyPackager extends Packager {
   static get fields() {
-    return { a: { mandatory: true } };
+    return { a: { detault: "av", mandatory: true }, b: { default: "bv" } };
   }
 }
+
 test("packager fields", t => {
   const p = new MyPackager();
   t.truthy(p.fields.a);
 });
+
+test("packager properties", t => {
+  const p = new MyPackager({ a : 1 });
+  t.is(p.properties.a, 1);
+  t.is(p.properties.b, "bv" );
+} );

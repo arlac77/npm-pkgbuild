182d4841302f309284b90860d454bd28b4c8285d
Markus Felten <Markus.Felten@gmx.de>
2022-01-23 15:40:07 +0100

    fix: exclude even more files from node-modules

--- src/content/node-modules-content-provider.mjs
+++ src/content/node-modules-content-provider.mjs
@@ -53,11 +53,7 @@ export class NodeModulesContentProvider extends ContentProvider {
     for (const name of await globby("node_modules/**/*", {
       cwd: tmp
     })) {
-      if (
-        !name.match(
-          /(~|\.map|\.ts|\.orig|\.log|\.tmp|\.bak|\.bat|\.gypi|\.gyp|appveyor\.yml|yarn\.lock|\.DS_Store|jenkinsfile|\.travis\.yml|\.jshint(rc)?|\.npm.*|\.git.*|rollup\.config\.(js|mjs|cjs)|CODE_OF_CONDUCT(\.md)?|GOVERNANCE(\.md)?|CODEOWNERS(\.md)?|UPGRAD(E|ING)(\.md)?|AUTHORS(\.md)?|CONTRIBUT(ORS|ING)(.md)?|CHANGELOG(\.md)?|HISTORY(\.md)?|LICENSE(\-\w+|\.md|\.txt)?|README(.*\.md)?|\.o|\.a|\.c|\.cc|\.h|\.in|Makefile|\.cmake|\.mk|\.\d)$/i
-        )
-      ) {
+      if (!toBeSkipped.test(name)) {
         yield Object.assign(
           new FileSystemEntry(name, tmp),
           this.entryProperties
@@ -67,52 +63,91 @@ export class NodeModulesContentProvider extends ContentProvider {
   }
 }
 
-const toBeIgnored = [
-  {
-    pattern: [
-      "*.d.ts*",
-      "*.patch",
-      ".jshintrc*",
-      ".esl*",
-      ".zuul.yml",
-      ".doclets.yml",
-      ".editorconfig",
-      ".tern-project",
-      ".dockerignore",
-      ".dir-locals.el",
-      "gulpfile.js",
-      "jsdoc.json",
-      "Gruntfile.js",
-      "karma.conf.js",
-      "verb.md",
-      ".nvmrc",
-      "bower.json",
-      "*.bash_completion.*",
-      ".coveralls.yml",
-      ".istanbul.yml",
-      ".babelrc.*",
-      ".nycrc",
-      ".env",
-      "x-package.json5",
-      "component.json",
-      "tsconfig.json",
-      "cypress.json",
-      ".airtap.yml",
-      ".jscs.json",
-      "sauce-labs.svg",
-      "PATENTS*",
-      "NOTICE*",
-      "SUMMARY.md",
-      "MIGRAT*.md",
-      "PULL_REQUEST_TEMPLATE.md",
-      "PATTERNS.md",
-      "REFERENCE.md",
-      "SECURITY.md",
-      "SFTPStream.md",
-      "LIMITS.md",
-      "Porting-Buffer.md",
-      "chains and topics.md",
-      "CODE_OF_CONDUCT*"
-    ]
-  }
-];
+const toBeSkipped = new RegExp(
+  "(" +
+    [
+      "~",
+      "\\.map",
+      "\\.ts",
+      "\\.orig",
+      "\\.log",
+      "\\.tmp",
+      "\\.bak",
+      "\\.bat",
+      "\\.gypi",
+      "\\.gyp",
+      "appveyor\\.yml",
+      "yarn\\.lock",
+      "\\.DS_Store",
+      "jenkinsfile",
+      "\\.travis\\.yml",
+      "\\.jshint(rc)?",
+      "\\.npm.*",
+      "\\.git.*",
+      "rollup\\.config\\.(js|mjs|cjs)",
+      "CODE_OF_CONDUCT(\\.md)?",
+      "GOVERNANCE(\\.md)?",
+      "CODEOWNERS(\\.md)?",
+      "UPGRAD(E|ING)(\\.md)?",
+      "AUTHORS(\\.md)?",
+      "CONTRIBUT(ORS|ING)(\\.md)?",
+      "CHANGELOG(\\.md)?",
+      "HISTORY(\\.md)?",
+      "LICENSE(-w+|\\.md|\\.txt)?",
+      "README(.*\\.md)?",
+      "\\.o",
+      "\\.a",
+      "\\.c",
+      "\\.cc",
+      "\\.h",
+      "\\.in",
+      "Makefile",
+      "\\.cmake",
+      "\\.mk",
+      "\\.d",
+
+      "\\.patch",
+      "\\.esl*",
+      "\\.zuul\\.yml",
+      "\\.doclets\\.yml",
+      "\\.editorconfig",
+      "\\.tern-project",
+      "\\.dockerignore",
+      "\\.dir-locals\\.el",
+      "gulpfile\\.js",
+      "jsdoc\\.json",
+      "Gruntfile\\.js",
+      "karma\\.conf\\.js",
+      "verb\\.md",
+      "\\.nvmrc",
+      "bower\\.json",
+      "\\.bash_completion.*",
+      "\\.coveralls\\.yml",
+      "\\.istanbul\\.yml",
+      "\\.babelrc.*",
+      "\\.nycrc",
+      "\\.env",
+      "x-package\\.json5",
+      "component\\.json",
+      "tsconfig\\.json",
+      "cypress\\.json",
+      "\\.airtap\\.yml",
+      "\\.jscs\\.json",
+      "sauce-labs\\.svg",
+      "PATENTS(\\.md)?",
+      "NOTICE(\\.md)?",
+      "SUMMARY\\.md",
+      "MIGRAT.*\\.md",
+      "PULL_REQUEST_TEMPLATE\\.md",
+      "PATTERNS\\.md",
+      "REFERENCE\\.md",
+      "SECURITY\\.md",
+      "SFTPStream\\.md",
+      "LIMITS\\.md",
+      "Porting-Buffer\\.md",
+      "chains and topics\\.md",
+    ].join("|") +
+    ")$",
+  "i"
+);
+

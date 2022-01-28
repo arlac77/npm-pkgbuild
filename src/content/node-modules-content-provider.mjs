e25185a91f4b02b51dded64de3e924ff4e6e4f7d
Markus Felten <Markus.Felten@gmx.de>
2022-01-28 01:51:03 +0100

    fix: skip win32 natif dirs

--- src/content/node-modules-content-provider.mjs
+++ src/content/node-modules-content-provider.mjs
@@ -171,6 +171,6 @@ const toBeSkipped = new RegExp(
       "chains and topics\\.md",
       "build_detect_platform"
     ].join("|") +
-    ")$|(node_modules\/(\@types|node\-addon\-api))",
+    ")$|(node_modules\/(\@types|node\-addon\-api)|win32\-(ia32|x64))",
   "i"
 );

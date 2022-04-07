b75aa057426a3e5889665e7793c3d5d78f71f857
Markus Felten <Markus.Felten@gmx.de>
2022-04-07 10:33:20 +0200

    test: copyFiles destinations

--- tests/copy-entries-test.mjs
+++ tests/copy-entries-test.mjs
@@ -3,11 +3,39 @@ import { access, mkdtemp, readFile } from "fs/promises";
 import { constants } from "fs";
 import { join } from "path";
 import { tmpdir } from "os";
-import { ReadableStreamContentEntry } from "content-entry";
+import { ReadableStreamContentEntry, StringContentEntry } from "content-entry";
 import { transform } from "content-entry-transform";
 import { keyValueTransformer } from "key-value-transformer";
 import { aggregateFifo } from "aggregate-async-iterator";
 import { FileContentProvider, copyEntries } from "npm-pkgbuild";
+
+
+test("copyEntries destination base dir", async t => {
+  async function * files() {
+    const a = new StringContentEntry("a");
+    a.destination = "/d1/d11/";
+    yield a;
+
+    const b = new StringContentEntry("b");
+    b.destination = "/d2/d21";
+    yield b;
+  }
+
+  const tmp = await mkdtemp(join(tmpdir(), "copy-"));
+
+  for await (const entry of copyEntries(files(), tmp)) {
+    switch(entry.name) {
+      case 'a': t.is(entry.destination,"/d1/d11/a");
+      break;
+      case 'b': t.is(entry.destination,"/d2/d21");
+      break;
+    }
+  }
+
+  await access(join(tmp, "d1/d11/a"), constants.F_OK);
+  await access(join(tmp, "d2/d21"), constants.F_OK);
+  t.true(true);
+});
 
 test("copyEntries plain", async t => {
   const files = new FileContentProvider({

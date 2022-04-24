9cc7ad1cfa4f2653e35ca865e8f9e3283776ce67
Markus Felten <Markus.Felten@gmx.de>
2022-04-24 16:14:55 +0200

    test: arch default properties

--- tests/arch-test.mjs
+++ tests/arch-test.mjs
@@ -4,6 +4,27 @@ import { stat, mkdtemp } from "fs/promises";
 import { tmpdir } from "os";
 import { FileContentProvider, ARCH } from "npm-pkgbuild";
 
+test("arch default properties", async t => {
+  const properties = {
+    name: "abc",
+    version: "1.0.0",
+    description: "a description",
+    license: "MIT"
+  };
+
+  const out = new ARCH(properties);
+
+  t.deepEqual(out.properties, {
+    ...properties,
+    epoch: 0,
+    arch: ["any"],
+    pkgdesc: properties.description,
+    pkgver: properties.version,
+    pkgname: properties.name,
+    release: 1
+  });
+});
+
 test("arch", async t => {
   const sources = ["fixtures/content", "fixtures/pkg"].map(source =>
     new FileContentProvider({
@@ -26,12 +47,9 @@ test("arch", async t => {
     "nginx-mainline": ">=1.21.4",
     konsum: ">=4.3.8"
   };
-  const fileName = await out.execute(
-    sources,
-    transformer,
-    dependencies,
-    { destination }
-  );
+  const fileName = await out.execute(sources, transformer, dependencies, {
+    destination
+  });
   t.is(fileName, join(destination, "abc-1.0.0-1-any.pkg.tar.zst"));
 
   const s = await stat(fileName);

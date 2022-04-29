b0750f296672abac84a6ead3481a073391ab26c4
Markus Felten <Markus.Felten@gmx.de>
2022-04-29 16:20:58 +0200

    test: file group / user

--- tests/file-content-provider-test.mjs
+++ tests/file-content-provider-test.mjs
@@ -2,37 +2,55 @@ import test from "ava";
 import { FileContentProvider } from "npm-pkgbuild";
 
 async function fcpt(t, definition, destination, list) {
-  const content = new FileContentProvider(definition,
-    typeof destination === "string" ? { destination } : destination);
+  const content = new FileContentProvider(
+    definition,
+    typeof destination === "string" ? { destination } : destination
+  );
 
   const entries = [];
   for await (const entry of content) {
     entries.push(entry);
   }
 
-  t.deepEqual(entries.map(e => { 
-    const r = { name: e.name, destination: e.destination };
-    if(e.user) { r.user = e.user; }
-    return r;
-  }), list);
+  t.deepEqual(
+    entries.map(e => {
+      const r = { name: e.name, destination: e.destination };
+      for (const a of ["user", "group", "mode"]) {
+        if (e[a]) {
+          r[a] = e[a];
+        }
+      }
+      return r;
+    }),
+    list
+  );
 
   const exists = await Promise.all(entries.map(e => e.isExistent));
   t.is(exists.filter(e => e).length, entries.length);
 }
-fcpt.title = (providedTitle = "FileContentProvider list", definition, destination, list) =>
-  ` ${providedTitle} ${JSON.stringify(
-    definition
-  )} -> ${JSON.stringify(list)}`.trim();
+fcpt.title = (
+  providedTitle = "FileContentProvider list",
+  definition,
+  destination,
+  list
+) =>
+  ` ${providedTitle} ${JSON.stringify(definition)} -> ${JSON.stringify(
+    list
+  )}`.trim();
 
-
-test(fcpt, new URL("fixtures/skeleton/package.json", import.meta.url).pathname,
-  { destination: "dest/package.json",
-    user: "root"
-   },
-  [{
-    name: "package.json", 
-    user: "root",
-    destination: "dest/package.json"}]
+test(
+  fcpt,
+  new URL("fixtures/skeleton/package.json", import.meta.url).pathname,
+  { destination: "dest/package.json", user: "root", group: "sys" },
+  [
+    {
+      name: "package.json",
+      user: "root",
+      group: "sys",
+      mode: 0o644,
+      destination: "dest/package.json"
+    }
+  ]
 );
 
 test(
@@ -41,7 +59,7 @@ test(
     base: new URL("fixtures/skeleton", import.meta.url).pathname
   },
   "dest",
-  [{name:"package.json",destination:"dest"}]
+  [{ name: "package.json", mode: 0o644, destination: "dest" }]
 );
 
 test(
@@ -51,7 +69,7 @@ test(
     pattern: "**/*.json"
   },
   "dest",
-  [{name:"package.json", destination: "dest"}]
+  [{ name: "package.json", mode: 0o644, destination: "dest" }]
 );
 
 test(
@@ -64,9 +82,4 @@ test(
   []
 );
 
-test(
-  fcpt,
-  "pacman/tmpfiles.conf",
-  "dest",
-  []
-);
+test(fcpt, "pacman/tmpfiles.conf", "dest", []);

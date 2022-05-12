2cdf252376108356d5c6b2e37b4d22ca5b7679c4
Markus Felten <Markus.Felten@gmx.de>
2022-05-12 14:26:58 +0200

    test: call publish several times

--- tests/publish-test.mjs
+++ tests/publish-test.mjs
@@ -1,10 +1,33 @@
 import test from "ava";
-import { analysePublish } from "../src/publish.mjs";
+import { analysePublish, publish } from "../src/publish.mjs";
 
 test("analysePublish", t => {
-  const publish = { url: "http://myserver.com/{{access}}/{{arch}}" };
+  const destination = { url: "http://myserver.com/{{access}}/{{arch}}" };
 
-  analysePublish(publish, { arch: "aarch64", access: "private" });
+  analysePublish(destination, { arch: "aarch64", access: "private" });
 
   t.is(publish.url, "http://myserver.com/private/aarch64");
+});
+
+test("publish twice", async t => {
+  const file = new URL("fixtures/content/file1.txt", import.meta.url).pathname;
+
+  const properties = { arch: "aarch64", access: "private" };
+  const destination = {
+    url: "http://myserver.com/{{type}}/{{access}}/{{arch}}"
+  };
+
+  try {
+    properties.type = "debian";
+    await publish(file, destination, properties);
+  } catch (e) {
+    t.true(e.toString().indexOf("http://myserver.com/debian/private/aarch64/file1.txt") >= 0);
+  }
+
+  try {
+    properties.type = "arch";
+    await publish(file, destination, properties);
+  } catch (e) {
+    t.true(e.toString().indexOf("http://myserver.com/arch/private/aarch64/file1.txt") >= 0);
+  }
 });

a173c9b09cbdcc901080602242b9265068ee3535
Markus Felten <Markus.Felten@gmx.de>
2022-06-17 15:45:52 +0200

    doc: wip

--- src/output/debian.mjs
+++ src/output/debian.mjs
@@ -179,3 +179,9 @@ const fields = {
   Uploaders: { mandatory: false },
   "Installed-Size": {}
 };
+
+/*
+@see https://www.debian.org/doc/debian-policy/ch-archive.html#sections
+sections:
+admin, cli-mono, comm, database, debug, devel, doc, editors, education, electronics, embedded, fonts, games, gnome, gnu-r, gnustep, graphics, hamradio, haskell, httpd, interpreters, introspection, java, javascript, kde, kernel, libdevel, libs, lisp, localization, mail, math, metapackages, misc, net, news, ocaml, oldlibs, otherosfs, perl, php, python, ruby, rust, science, shells, sound, tasks, tex, text, utils, vcs, video, web, x11, xfce, zope
+*/

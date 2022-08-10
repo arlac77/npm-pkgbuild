d0c1c03cf209dabd251da623619938ea8cabab76
Markus Felten <Markus.Felten@gmx.de>
2022-08-10 14:06:42 +0200

    fix: use pipeline to compress tar

--- src/output/oci.mjs
+++ src/output/oci.mjs
@@ -1,8 +1,14 @@
 import { join } from "node:path";
 import { createWriteStream } from "node:fs";
 import { createGzip } from "node:zlib";
+import { createHash } from "node:crypto";
+import { pipeline } from "node:stream/promises";
 import { aggregateFifo } from "aggregate-async-iterator";
 import { Packager } from "./packager.mjs";
+
+const MEDIA_TYPE_MANIFEST = "application/vnd.oci.image.manifest.v1+json";
+const MEDIA_TYPE_CONFIG = "application/vnd.oci.image.config.v1+json";
+const MEDI_TYPE_IMAGE_LAYER = "application/vnd.oci.image.layer.v1.tar+gzip";
 
 function into(buffer, offset, string) {
   for (let i = 0; i < string.length; i++) {
@@ -57,40 +63,37 @@ export class OCI extends Packager {
     const packageFile = join(destination, this.packageFileName);
 
     const meta = {
-      "schemaVersion": 2,
-      "mediaType": "application/vnd.oci.image.manifest.v1+json",
-      "config": {
-        "mediaType": "application/vnd.oci.image.config.v1+json",
-        "size": 7023,
-        "digest": "sha256:b5b2b2c507a0944348e0303114d8d93aaaa081732b86451d9bce1f432a537bc7"
+      schemaVersion: 2,
+      mediaType: MEDIA_TYPE_MANIFEST,
+      config: {
+        mediaType: MEDIA_TYPE_CONFIG,
+        size: 7023,
+        digest:
+          "sha256:b5b2b2c507a0944348e0303114d8d93aaaa081732b86451d9bce1f432a537bc7"
       },
-      "layers": [
+      layers: [
         {
-          "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
-          "size": 32654,
-          "digest": "sha256:9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0"
-        },
-        {
-          "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
-          "size": 73109,
-          "digest": "sha256:ec4b8955958665577945c89419d1af06b5f7636b4ac3da7f12184802ad867736"
+          mediaType: MEDI_TYPE_IMAGE_LAYER,
+          size: 73109,
+          digest:
+            "sha256:ec4b8955958665577945c89419d1af06b5f7636b4ac3da7f12184802ad867736"
         }
-      ], 
-      "annotations": {
-        description: properties.description
+      ],
+      annotations: {
+        description: properties.description,
+        version: properties.version
       }
     };
 
     const out = createGzip();
-
-    out.pipe(createWriteStream(packageFile));
+    const hash = createHash("sha256");
 
     console.log(packageFile);
 
     const header = new Uint8Array(512);
     into(header, 257, "ustar");
-    header[263] = 48 // 0;
-    header[264] = 48 // 0;
+    header[263] = 48; // 0;
+    header[264] = 48; // 0;
 
     intoOctal(header, 108, 8, 0 /* root */);
     intoOctal(header, 116, 8, 3 /* sys */);
@@ -112,7 +115,6 @@ export class OCI extends Packager {
       intoOctal(header, 148, 8, chksum(header));
 
       out.write(header);
-
       out.write(buffer);
 
       /*
@@ -126,15 +128,17 @@ export class OCI extends Packager {
 
       const allign = 512 - (size % 512);
 
-      //      console.log(entry.name, size, buffer.length,allign);
-
       if (allign > 0) {
         out.write(new Uint8Array(allign));
       }
     }
 
-    const filler = new Uint8Array(1024*8);
+    const filler = new Uint8Array(1024 * 8);
     out.end(filler);
+
+    await pipeline(out, createWriteStream(packageFile));
+
+    console.log(hash.digest("base64"));
 
     return packageFile;
   }
--- tests/oci-test.mjs
+++ tests/oci-test.mjs
@@ -2,6 +2,10 @@ import test from "ava";
 import { join } from "node:path";
 import { stat, mkdtemp } from "node:fs/promises";
 import { tmpdir } from "node:os";
+import { createReadStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { extract } from "tar-stream";
 import { FileContentProvider, OCI } from "npm-pkgbuild";
 
 test("oci", async t => {
@@ -32,4 +36,25 @@ test("oci", async t => {
 
   const s = await stat(fileName);
   t.true(s.size >= 10, `package file size (${s.size})`);
+
+  const ex = extract();
+
+  const entries = {};
+
+  ex.on("entry", async (header, stream, next) => {
+    stream.on("end", () => next());
+
+    let bytes = 0;
+    for await (const chunk of await stream) {
+      bytes += chunk.length;
+    }
+
+    entries[header.name] = header;
+  //  console.log(header.name, bytes);
+  });
+
+  await pipeline(createReadStream(fileName), createGunzip(), ex);
+
+  t.is(entries["file1.txt"].size, 93);
+  t.is(entries["file2.json"].size, 23);
 });

c54eb1a0f4780cd3aab9613cb6bc0c38b2b60ab1
Markus Felten <Markus.Felten@gmx.de>
2023-01-20 18:05:55 +0100

    fix: typo

--- src/content/node-modules-content-provider.mjs
+++ src/content/node-modules-content-provider.mjs
@@ -135,7 +135,7 @@ const toBeSkipped = new RegExp(
       "\\.ts",
       "\\.mts",
       "\\.d\\.ts",
-      "\\.debug\\.js"
+      "\\.debug\\.js",
       "\\.coffee",
       "tsdoc-metadata\\.json",
       "\\.orig",

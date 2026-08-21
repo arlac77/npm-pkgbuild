72722ccbbb6d7676b37bb99b40dce77ee90079f7
Markus Felten <Markus.Felten@gmx.de>
2026-08-21 23:50:03 +0100

    fix: handle more cases for asArray

--- src/util.mjs
+++ src/util.mjs
@@ -138,8 +138,26 @@ export function quote(v, qc = "'") {
     return v.match(/^\w+$/) ? v : qc + v + qc;
 }
 
-export function asArray(o) {
-  return Array.isArray(o) ? o : o ? [o] : [];
+/**
+ * Convert scalar or iterable into Array.
+ * @param {any} value
+ * @returns {Array<any>}
+ */
+export function asArray(value) {
+  switch (typeof value) {
+    case "undefined":
+      return [];
+    case "object":
+      if (Array.isArray(value)) {
+        return value;
+      }
+
+      if (value instanceof Iterator || value[Symbol.iterator]) {
+        return [...value];
+      }
+  }
+
+  return [value];
 }
 
 /**
